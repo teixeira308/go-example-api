abe41f1ec9c3eba63969604391af5602cc3d67e9
Guilherme Teixeira <teixeira308@hotmail.com>
2025-07-19 03:01:23 -0300

    adding .env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,30 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080" // fallback
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: port,
 	}
 	app := &application{
 		config: cfg,
 	}
+
 	log.Printf("Server at %s", app.config.addr)
 	mux := app.mount()
 	log.Fatal(app.run(mux))
